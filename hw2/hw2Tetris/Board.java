fcd7bdc7451cb643c7ced6eec908527addb48d82
dex11 <nick.jebirashvili@gmail.com>
2020-04-16 10:28:55 -0800

    'v354'

--- hw2/hw2Tetris/Board.java
+++ hw2/hw2Tetris/Board.java
@@ -1,5 +1,7 @@
 // Board.java
 
+import java.util.Arrays;
+
 /**
  CS108 Tetris Board.
  Represents a Tetris board -- essentially a 2-d grid
@@ -14,7 +16,8 @@ public class Board	{
 	private int height;
 	private int widths[];
 	private int heights[];
-	private boolean[][] grid;
+	public boolean[][] grid;
+	private boolean[][]posGrid;
 	private boolean DEBUG = true;
 	boolean committed;
 	
@@ -29,6 +32,7 @@ public class Board	{
 		this.width = width;
 		this.height = height;
 		grid = new boolean[width][height];
+		posGrid = new boolean[width][height];
 		committed = true;
 		
 		this.widths = new int[height];
@@ -76,8 +80,32 @@ public class Board	{
 	 for debugging.
 	*/
 	public void sanityCheck() {
+//		System.out.println("before san");
+//		System.out.println(Arrays.toString(this.grid[0]));
+//		System.out.println(Arrays.toString(this.grid[1]));
+//		System.out.println(Arrays.toString(this.grid[2]));
 		if (DEBUG) {
-			// YOUR CODE HERE
+			int[] compHeights = new int[this.width];
+			int[] compWidths = new int[this.height];
+			for(int row = 0; row < this.width; row++) {
+				int curHeight = 0;
+				for (int i = 0; i < this.height; i++) {
+					if(this.grid[row][i]) curHeight = i + 1 ;
+				}
+				compHeights[row] = curHeight;
+			}
+//			System.out.println("COMP HEIGHTS " + Arrays.toString(compHeights) +  " TO " + Arrays.toString(this.heights));
+
+			for (int i = 0; i < this.height; i++) {
+				int curWidth = 0;
+				for(int row = 0; row < this.width; row++) {
+					if(this.grid[row][i]) curWidth++;
+				}
+				compWidths[i] = curWidth;
+			}
+//			System.out.println("COMP WIDTH " + Arrays.toString(compWidths) +  " TO " + Arrays.toString(this.widths));
+			if(!Arrays.equals(this.heights, compHeights)) throw new RuntimeException("WRONG HEIGHTS");
+			if(!Arrays.equals(this.widths, compWidths)) throw new RuntimeException("WRONG WIDTHS");
 		}
 	}
 	
@@ -91,7 +119,15 @@ public class Board	{
 	 to compute this fast -- O(skirt length).
 	*/
 	public int dropHeight(Piece piece, int x) {
-		return 0; // YOUR CODE HERE
+		int stopY = 0;
+		int y = 0;
+		for(int i = 0; i < piece.getWidth(); i++){
+			if(this.heights[x + i] + piece.getSkirt()[i] > stopY){
+				stopY = this.heights[x + i] + piece.getSkirt()[i];
+				y = stopY + (piece.getSkirt()[i] - piece.getSkirt()[0] - 1);
+			}
+		}
+		return y;
 	}
 	
 	
@@ -101,7 +137,8 @@ public class Board	{
 	 The height is 0 if the column contains no blocks.
 	*/
 	public int getColumnHeight(int x) {
-		return 0; // YOUR CODE HERE
+		int ans = this.heights[x];
+		return ans;
 	}
 	
 	
@@ -110,7 +147,7 @@ public class Board	{
 	 the given row.
 	*/
 	public int getRowWidth(int y) {
-		 return 0; // YOUR CODE HERE
+		 return this.widths[y]; // YOUR CODE HERE
 	}
 	
 	
@@ -120,7 +157,8 @@ public class Board	{
 	 always return true.
 	*/
 	public boolean getGrid(int x, int y) {
-		return false; // YOUR CODE HERE
+		if(x >= this.width || y >= this.height) return true;
+		return this.grid[x][y];
 	}
 	
 	
@@ -146,29 +184,98 @@ public class Board	{
 	public int place(Piece piece, int x, int y) {
 		// flag !committed problem
 		if (!committed) throw new RuntimeException("place commit problem");
-
-		for(int i = 0; i < piece.getBody().length; i++){
-			if(heights[x + i] > y) return PLACE_BAD;
-			if(heights[x + i] > this.height || (x + i) > this.width || (x + i) < 0) return PLACE_OUT_BOUNDS;
-
+		gridClon(grid, posGrid);
+		TPoint[] body = piece.getBody();
+		for(int i = 0; i < piece.getBody().length; ++i){
+			int pX = body[i].x;
+			int pY = body[i].y;
+			if(x + pX >= this.width || y + pY >= this.height || x + pX < 0 || y + pY < 0){
+				committed = false;
+				return PLACE_OUT_BOUNDS;
+			}
+			if(this.grid[x + pX][y + pY]){
+				committed = false;
+				return PLACE_BAD;
+			}
+			//this.heights[x + pX] > y + piece.getSkirt()[pX] + 1 ||
+			this.grid[x + pX][y + pY] = true;
+			this.widths[y + pY]++;
+			this.heights[x + pX]++;
+			if(this.widths[y + pY] == this.width){
+				committed = false;
+				return PLACE_ROW_FILLED;
+			}
 		}
-
+		committed = false;
 		return PLACE_OK;
 	}
-	
-	
+
+	private void gridClon(boolean[][] src, boolean[][]dest){
+		for(int row = 0; row < this.width; row++) {
+			for (int i = 0; i < this.height; i++) {
+				dest[row][i] = src[row][i];
+			}
+		}
+	}
+
 	/**
 	 Deletes rows that are filled all the way across, moving
 	 things above down. Returns the number of rows cleared.
 	*/
 	public int clearRows() {
 		int rowsCleared = 0;
-		// YOUR CODE HERE
+		gridClon(grid, posGrid);
+		for(int col = 0; col < this.height; ++col){
+			boolean isGood = true;
+			for(int row = 0; row < this.width; row++){
+				if(this.grid[row][col] == false) isGood = false;
+			}
+			if(isGood){
+				moveCol(col);
+				rowsCleared++;
+				col = col - 1;
+
+			}
+		}
+		updateHeights();
+		updateWidth();
+		committed = false;
 		sanityCheck();
 		return rowsCleared;
 	}
 
+	private void updateHeights(){
+		for(int row = 0; row < this.width; row++) {
+			int curHeight = 0;
+			for (int i = 0; i < this.height; i++) {
+				if(this.grid[row][i]) curHeight = i + 1 ;
+			}
+			this.heights[row] = curHeight;
+		}
+	}
 
+	private void updateWidth(){
+		for (int i = 0; i < this.height; i++) {
+			int curWidth = 0;
+			for(int row = 0; row < this.width; row++) {
+				if(this.grid[row][i]) curWidth++;
+			}
+			this.widths[i] = curWidth;
+		}
+	}
+
+	private void moveCol( int col){
+		for(int row = 0; row < this.width; row++){
+			for(int i = col + 1; i < this.height; i++){
+				this.grid[row][i - 1] = this.grid[row][i];
+				this.widths[i-1] = this.widths[i];
+			}
+		}
+		//add free spaces at the end
+		for(int row = 0; row < this.width; row++){
+			this.grid[row][this.height - 1] = false;
+		}
+	}
 
 	/**
 	 Reverts the board to its state before up to one place
@@ -178,10 +285,15 @@ public class Board	{
 	 See the overview docs.
 	*/
 	public void undo() {
-		// YOUR CODE HERE
+		if (!committed) {
+			gridClon(posGrid, grid);
+			updateHeights();
+			updateWidth();
+			committed = true;
+		}
 	}
-	
-	
+
+
 	/**
 	 Puts the board in the committed state.
 	*/
@@ -210,6 +322,8 @@ public class Board	{
 		for (int x=0; x<width+2; x++) buff.append('-');
 		return(buff.toString());
 	}
+
+
 }
 
 
--- hw2/hw2Tetris/BoardTest.java
+++ hw2/hw2Tetris/BoardTest.java
@@ -1,5 +1,7 @@
 import junit.framework.TestCase;
 
+import java.util.Arrays;
+
 
 public class BoardTest extends TestCase {
 	Board b;
@@ -19,10 +21,9 @@ public class BoardTest extends TestCase {
 		pyr2 = pyr1.computeNextRotation();
 		pyr3 = pyr2.computeNextRotation();
 		pyr4 = pyr3.computeNextRotation();
-		
+
 		s = new Piece(Piece.S1_STR);
 		sRotated = s.computeNextRotation();
-		
 		b.place(pyr1, 0, 0);
 	}
 	
@@ -35,9 +36,14 @@ public class BoardTest extends TestCase {
 		assertEquals(1, b.getRowWidth(1));
 		assertEquals(0, b.getRowWidth(2));
 	}
+	public void testSample2() {
+		b.commit();
+		int drop = b.dropHeight(sRotated, 1);
+		assertEquals(2, drop);
+	}
 	
 	// Place sRotated into the board, then check some measures
-	public void testSample2() {
+	public void testSample3() {
 		b.commit();
 		int result = b.place(sRotated, 1, 1);
 		assertEquals(Board.PLACE_OK, result);
@@ -46,10 +52,54 @@ public class BoardTest extends TestCase {
 		assertEquals(3, b.getColumnHeight(2));
 		assertEquals(4, b.getMaxHeight());
 	}
-	
+
+
+
+	public void testSample4() {
+		b.commit();
+		int result = b.place(sRotated, 1, 1);
+		int n = b.clearRows();
+		assertEquals(1, n);
+	}
+
+	public void testSample5() {
+		b.commit();
+		int result = b.place(sRotated, 1, 1);
+		b.undo();
+		int ans = b.clearRows();
+		b.commit();
+		assertEquals(1, ans);
+		assertEquals(0, b.getColumnHeight(0));
+		assertEquals(1, b.getColumnHeight(1));
+		assertEquals(0, b.getColumnHeight(2));
+	}
 	// Makre  more tests, by putting together longer series of 
 	// place, clearRows, undo, place ... checking a few col/row/max
 	// numbers that the board looks right after the operations.
-	
+
+	public void testSample6() {
+		b.commit();
+		int result = b.place(sRotated, 1, 1);
+		b.commit();
+		System.out.println(Arrays.toString(b.grid[0]));
+		System.out.println(Arrays.toString(b.grid[1]));
+		System.out.println(Arrays.toString(b.grid[2]));
+		assertEquals(result, Board.PLACE_OK);
+		b.commit();
+		int result1 = b.place(sRotated, 1, 1);
+		System.out.println(" ");
+		System.out.println(Arrays.toString(b.grid[0]));
+		System.out.println(Arrays.toString(b.grid[1]));
+		System.out.println(Arrays.toString(b.grid[2]));
+		assertEquals(result1, Board.PLACE_BAD);
+		b.undo();
+		b.commit();
+		int result2 = b.place(sRotated, 1, 5);
+		System.out.println(" ");
+		System.out.println(Arrays.toString(b.grid[0]));
+		System.out.println(Arrays.toString(b.grid[1]));
+		System.out.println(Arrays.toString(b.grid[2]));
+		assertEquals(result2, Board.PLACE_OUT_BOUNDS);
+	}
 	
 }
