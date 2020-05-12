0b282ee6749747a2bc634b74e203278d5b1117c0
dex11 <nick.jebirashvili@gmail.com>
2020-05-12 14:59:00 +0400

    woop

--- hw3/src/Sudoku.java
+++ hw3/src/Sudoku.java
@@ -139,14 +139,18 @@ public class Sudoku {
 	
 	private List<Spot> board;
 	private List< Pair<Spot, Integer> > spots;
+	private long startTime;
+	private long endTime;
 	/**
 	 * Sets up based on the given ints.
 	 */
 	public Sudoku(int[][] ints) {
+		startTime = System.currentTimeMillis();
+		endTime = 0;
 		board = new ArrayList<>();
 		for(int i = 0; i < ints.length; i++){
 			for(int j = 0; j < ints[0].length; j++){
-				Spot s = new Spot(ints[i][j], i + j);
+				Spot s = new Spot(ints[i][j], i*9 + j);
 				board.add(s);
 			}
 		}
@@ -155,39 +159,60 @@ public class Sudoku {
 	}
 	
 	private void creatSpots(){
+		spots = new ArrayList<>();
 		for(int i = 0; i < board.size(); i++){
 			int cur = 0;
 			Spot s = board.get(i);
-			if(s.get() == 0) cur = s.getValues(board, i).size();
-			int index = 0;
-			for(int j = 0; j < spots.size(); j++){
-				if(spots.get(i).getValue() > cur){
-					index = j;
-					break;
+			if(s.get() == 0) {
+//				System.out.print(" " + s.index+"-" +i);
+				cur = s.getValues(board, i).size();
+				int index = 0;
+				for (int j = 0; j < spots.size(); j++) {
+					if (spots.get(j).getValue() > cur) {
+						index = j;
+						break;
+					}
+					index++;
 				}
+				spots.add(index, new Pair<Spot, Integer>(s, cur));
 			}
-			spots.add(index, new Pair<Spot, Integer>(s, cur));
 		}
+		///just for test
+//		System.out.println();
+		for(int i = 0; i < spots.size(); i++){
+			System.out.print(" " + spots.get(i).getKey().getIndex() + "-" + spots.get(i).getValue());
+		}
+		System.out.println();
 	}
 	
 	/**
 	 * Solves the puzzle, invoking the underlying recursive search.
 	 */
 	public int solve() {
-		return  recSolve(0, 0);
+		int ans = recSolve(0, 0);
+		endTime = System.currentTimeMillis();
+		return  ans;
 	}
 
 	private int recSolve( int index ,int ans){
-		if(index == spots.size()) return 1;
+//		System.out.print(" " + index);
+		if(index == spots.size()){
+//			System.out.println("WOOOHOOOOO FOUND ONE ");
+			return ans += 1;
+		}
 		Spot s = spots.get(index).getKey();
+//		System.out.print(" REAL INDEX " + s.getIndex() + " ");
 		List<Integer> vals = s.getValues(board, s.getIndex());
 		if(vals == null){
+//			System.out.println("WOOPS");
 			return 0;
 		}
-
+//		System.out.println();
+		int prev = ans;
 		for(int i = 0; i < vals.size(); i++){
+//			System.out.println(" " + i);
 			s.set(vals.get(i));
-			ans += recSolve(index + 1, ans);
+			ans += recSolve(index + 1, prev);
 		}
 		s.set(0);
 		return ans;
@@ -198,7 +223,7 @@ public class Sudoku {
 	}
 	
 	public long getElapsed() {
-		return 0; // YOUR CODE HERE
+		return endTime - startTime; // YOUR CODE HERE
 	}
 
 	private class Spot{
@@ -224,31 +249,42 @@ public class Sudoku {
 		}
 
 		public List<Integer> getValues(List<Spot> board, int index){
+//			if(index == 76) System.out.println("SPECIAL CASE \n");
 			List<Integer> vals = new ArrayList<>();
 			for(int i = 1; i <= 9; i++){
 				vals.add(i);
 			}
 
+//			System.out.println("LENGTH " + board.size());
+//			System.out.println("INDEX " + index);
 			int row = index / 9;
-			int startOfRect = (row - (row % 3)) * 9;
+//			System.out.println("ROW " + row);
+			int startOfX = index - (index % 9);
+			int startOfY = index % 9;
+			int startOfRect = (row - (row % 3)) * 9 + (startOfY - (startOfY % 3));
 			for(int i = 0; i < 3; i++){
 				for(int j = 0; j < 3; j++){
+					if(vals.size() == 0) return null;
+//					System.out.println("POS INDEX " + ((startOfRect + 9*i) + j) );
 					Spot s = board.get((startOfRect + 9*i) + j);
-					vals.remove(s.get());
+//					if(index == 76 && s.get() != 0) System.out.println(s.get());
+					vals.remove(new Integer(s.get()));
 				}
 			}
 
-			int startOfX = index - (index % 9);
-
 			for(int i = 0; i < 9; i++){
+				if(vals.size() == 0) return null;
 				Spot s = board.get(startOfX + i);
-				vals.remove(s.get());
+//				if(index == 76 && s.get() != 0) System.out.println(s.get());
+				vals.remove(new Integer(s.get()));
 			}
 
-			int startOfY = index % 9;
+
 			for(int i = 0; i < 9; i++){
+				if(vals.size() == 0) return null;
 				Spot s = board.get(startOfY + 9*i);
-				vals.remove(s.get());
+//				if(index == 76 && s.get() != 0) System.out.println(s.get());
+				vals.remove(new Integer(s.get()));
 			}
 			if(vals.size() != 0) return vals;
 			return null;
